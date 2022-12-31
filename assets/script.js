f26259df7cb6a851d8170f7b02d4f747ea7071fc
Michael Reyna <25.27.reyna@gmail.com>
2022-12-31 00:32:31 -0700

    added the alert have lost time fnctn

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+function test(button) {
+    var questionOne = setTimeout(function() {
+        userChoice 
+        if (userChoice && correctAnswer){
+            document
+        } else {
+            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
+            return;
+            }}, 1000 * 30)
+    var questionTwo = function() {
+        if (userChoice && correctAnswer){
+
+        } else {
+            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
+            return;
+            }}
+    var questionThree = setTimeout(function () {
+        if (userChoice && correctAnswer){
+
+        } else {
+            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
+            return;
+        }}, 1000 * 30)
+}
+
+
+test()
