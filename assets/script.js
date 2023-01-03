54c89980b9377c367952f8538cc1aef45ace8015
Michael Reyna <25.27.reyna@gmail.com>
2023-01-02 18:29:33 -0700

    figuring out timer display

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,36 @@
 function test(button) {
+    var timer = 30
     var questionOne = setTimeout(function() {
-        userChoice 
+        // create a display timer that shows time you have to finish question
+        // create a check box list with html list elemnt
+        document.getElementById('count').innerHTML=timer; 
+        timer--;
+        function userChoice(userInput){
+            
+        }
         if (userChoice && correctAnswer){
             document
         } else {
             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
             return;
-            }}, 1000 * 30)
-    var questionTwo = function() {
-        if (userChoice && correctAnswer){
+            }
+        if (count === 0 ){
+            alert("You are out of time!")
+        }
+        }, 1000);
+//     var questionTwo = setTimeout(function() {
+//         if (userChoice && correctAnswer){
 
-        } else {
-            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
-            return;
-            }}
-    var questionThree = setTimeout(function () {
-        if (userChoice && correctAnswer){
+//         } else {
+//             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
+//             return;
+//             }}, 1000 * 30);
+//     var questionThree = setTimeout(function () {
+//         if (userChoice && correctAnswer){
 
-        } else {
-            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
-            return;
-        }}, 1000 * 30)
-}
+//         } else {
+//             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
+//             return;
+//         }}, 1000 * 30);
+// }
 
-
-test()
