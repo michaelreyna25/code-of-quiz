e057e73d1ced2a4a6eb13f0030c49da7b6c98df5
Michael Reyna <25.27.reyna@gmail.com>
2023-01-03 09:53:38 -0700

    added comments of what I need to do

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,31 @@
+var score = 0
+
 function test(button) {
     var timer = 30
     var questionOne = setTimeout(function() {
         // create a display timer that shows time you have to finish question
         // create a check box list with html list elemnt
+
+        // questions
+        var questions = ["Dogs can fly", "Cats can fly","Pigs can fly","Dogs can't fly"]
+        correctAnswer = questions("Dogs can't fly]")
+        // Timer
         document.getElementById('count').innerHTML=timer; 
         timer--;
+        // User choice
         function userChoice(userInput){
-            
+
         }
-        if (userChoice && correctAnswer){
-            document
+        // fix
+        if (count === 0 ){
+            alert("You are out of time!")
+        }else if (userChoice && correctAnswer){
+            alert("Correct!")
+            nexQuestion
         } else {
             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
             return;
-            }
-        if (count === 0 ){
-            alert("You are out of time!")
-        }
-        }, 1000);
+        }}, 1000);
 //     var questionTwo = setTimeout(function() {
 //         if (userChoice && correctAnswer){
 
@@ -32,5 +40,5 @@ function test(button) {
 //             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
 //             return;
 //         }}, 1000 * 30);
-// }
+}
 
