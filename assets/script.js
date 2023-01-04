70f0b14a16cc357e4d8754aafc7944e2e5d36a25
Michael Reyna <25.27.reyna@gmail.com>
2023-01-04 14:33:50 -0700

    removed current js and replaced with psuedo code

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,36 @@
-var score = 0
+var startButtonEl = $('#start-button')
+var score = 0;
+var win;
+var lose;
 
-function test(button) {
-    var timer = 30
-    var questionOne = setTimeout(function() {
-        // create a display timer that shows time you have to finish question
-        // create a check box list with html list elemnt
+function start(event){
+    event.preventDefault()
+    timer = 30
+    timer--;
 
-        // questions
-        var questions = ["Dogs can fly", "Cats can fly","Pigs can fly","Dogs can't fly"]
-        correctAnswer = questions("Dogs can't fly]")
-        // Timer
-        document.getElementById('count').innerHTML=timer; 
-        timer--;
-        // User choice
-        function userChoice(userInput){
+    if (timer === 0 ){
+    clear.event()
+    alert("You are out of time!")
+    }
+} 1000;
 
-        }
-        // fix
-        if (count === 0 ){
-            alert("You are out of time!")
-        }else if (userChoice && correctAnswer){
-            alert("Correct!")
-            nexQuestion
-        } else {
-            setTimeout(function() {alert("You have lost time!")}, 1000/5 );
-            return;
-        }}, 1000);
-//     var questionTwo = setTimeout(function() {
-//         if (userChoice && correctAnswer){
 
-//         } else {
-//             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
-//             return;
-//             }}, 1000 * 30);
-//     var questionThree = setTimeout(function () {
-//         if (userChoice && correctAnswer){
 
-//         } else {
-//             setTimeout(function() {alert("You have lost time!")}, 1000/5 );
-//             return;
-//         }}, 1000 * 30);
-}
 
+      
+
+// start function
+    //start timer 
+    //display question
+    //check answer
+    //end
+    //playagin
+// Timer function
+// display question function
+    //array of questions
+    //true or false
+// checkAnswer function
+// end function
+    //show score
+    //initials
+//play again function
