b825402fe23fc7ecbeb65fec49918c46aeb07c61
Michael Reyna <25.27.reyna@gmail.com>
2023-01-04 16:30:54 -0700

    finished most of the timer funct

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,45 @@
 var startButtonEl = $('#start-button')
 var displayTime = $('#timer')
 var displayScoreEl = $('#score')
-var bodyBoxEl = $('.box')
+var bodyBoxEl = $('#box')
 
 var score = 0;
 var win;
 var lose;
+var timer
+var time = 5;
 
-function start(event){
-    event.preventDefault()
-    timer = 30
-    timer--;
 
-    if (timer === 0 ){
-    clear.event()
-    alert("You are out of time!")
+startButtonEl.on("click", function() {
+    timerDisplay()
+    
+
+})
+
+var timerDisplay = setInterval(function(){
+    time--;
+    if (time > 0 ){
+        displayTime.text(time)
+    } else {
+        time === 0
+        displayTime.text(time)
+        alert("Game Over")
+        clearInterval(timerDisplay)
     }
-} 1000;
+}, 1000);
 
-
-
+var displayQuestions =
 
 // Vars      
 
-// start function
+// start btn function
     //start timer 
     //display question
     //check answer
     //end
     //playagin
 // Timer function
+    //event click when button is clicked
 // display question function
     //array of questions
     //true or false
