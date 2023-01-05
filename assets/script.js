6ce64cd1065c14028b81ab1d5cdc60336ae0e920
Michael Reyna <25.27.reyna@gmail.com>
2023-01-04 17:20:12 -0700

    added two funct pertained to question display and win

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,81 @@
+// <--- JavaScript --->
+
+// Selectors
 var startButtonEl = $('#start-button')
 var displayTime = $('#timer')
 var displayScoreEl = $('#score')
 var bodyBoxEl = $('#box')
+var questionDisplayEl = $('#display-questions')
 
-var score = 0;
-var win;
-var lose;
-var timer
+// Variables
 var time = 5;
+var score = 0;
+var win = 0;
+var lose = 0;
+var displayQuestions;
+var checkAnswer;
+var enndGame;
+var playAgain;
+var timerDisplay;
+var questionNumber = 0
 
+var questions = [{
+    quesion: "What is the color of the sky?",
+    userChoices: ["Blue","Red","Green","Brown"],
+    correctAnswer: 0 
+    },
+    {
+    question: "Do dogs fly?",
+    userChoices: ["Yes","No"],
+    correctAnswer: 1
+    },
+    {
+    question: "What color is snow?",
+    userChoices: ["Black","Yellow","Red","White"],
+    correctAnswer: 3
+}]
 
 startButtonEl.on("click", function() {
     timerDisplay()
-    
-
+    questionDisplay()
+    checkAnswer()
+    enndGame()
+    playAgain()
 })
 
-var timerDisplay = setInterval(function(){
+timerDisplay = setInterval(function(){
+    time()
     time--;
     if (time > 0 ){
         displayTime.text(time)
     } else {
         time === 0
+        lose++;
         displayTime.text(time)
         alert("Game Over")
         clearInterval(timerDisplay)
     }
 }, 1000);
 
-var displayQuestions =
+$(questionDisplayEl).on(function(){
+    
+    var userInput = [userChoices[i]]
+
+})
+
+function winGame() {
+    win++;
+    clearInterval(timerDisplay);
+}
+
+
 
 // Vars      
 
 // start btn function
     //start timer 
     //display question
+
     //check answer
     //end
     //playagin
